95edea2cbcd57ba322da2e2a2f17db90527db492
Salahjb <salah8jab@gmail.com>
2025-01-12 21:06:31 +0100

    first commit

---
+++ src/main/java/com/project/inssurancemanagement/InssuranceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.inssurancemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InssuranceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InssuranceManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/inssurancemanagement/InssuranceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.inssurancemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InssuranceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
