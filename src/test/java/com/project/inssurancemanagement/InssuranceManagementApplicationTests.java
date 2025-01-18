3475f5fc4bef1969e3d1b7726716a1e8bcedf005
mouad4949 <mouadrguibi900@gmail.com>
2025-01-18 21:55:43 +0100

    yarbi

--- src/test/java/com/project/inssurancemanagement/InssuranceManagementApplicationTests.java
+++ src/test/java/com/project/inssurancemanagement/InssuranceManagementApplicationTests.java
@@ -1,10 +1,17 @@
 package com.project.inssurancemanagement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 @ActiveProfiles("test")
 class InssuranceManagementApplicationTests {
 
